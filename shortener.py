be5ffe33a81aa1608a00c01dab5a8b5f1ad464d7
jacki0 <jackio@tuta.io>
2019-12-23 03:04:33 +0300

    update reduce

--- shortener.py
+++ shortener.py
@@ -6,18 +6,18 @@ import string
 def is_valid(message):
     if message[:8] != 'https://' and message[:7] != 'http://':        
         message = 'http://' + message
+    elif message[:8] == 'https://':
+        message.replace(message[5], '')
     try:
         urllib.request.urlopen(message)              # проверка валидности ссылки
-        return message
+        return message[message.index('/') + 2 : message.index('.')]
     except Exception:
         return False
-
 def reduce(message):
     message = is_valid(message)
     if message == False:
         return 'Ваша ссылка некорректна'
-    message = message[message.index('/') + 2 : message.index('.')]
-    if len(message) >= 3:
-        message = message[:random.randint(2, 4)]
-    message = 'tgshr.tk/' + message + ''.join(random.sample(string.ascii_letters + string.digits, random.randint(1, 3)))
-    return message
+    elif len(message) >= 3:
+        message = message[:3]
+    message = message + ''.join(random.sample(string.ascii_lowercase + string.digits, random.randint(1, 10)))
+    return 'tgshr.tk/' + message[:6]
