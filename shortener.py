abd896bbaadb7d007420eccc3148e500cf6a5c21
jacki0 <jackio@tuta.io>
2020-12-01 00:38:52 +0300

    change url and some fix

--- shortener.py
+++ shortener.py
@@ -4,20 +4,24 @@ import random
 import string
 
 def is_valid(message):
-    if message[:8] != 'https://' and message[:7] != 'http://':        
+    if  message[:4] != 'http':        
         message = 'http://' + message
     elif message[:8] == 'https://':
         message.replace(message[5], '')
     try:
+        print(message)
         urllib.request.urlopen(message)              # проверка валидности ссылки
-        return message[message.index('/') + 2 : message.index('.')]
+        return message[message.index('/') + 2 :]
     except Exception:
         return False
+
 def reduce(message):
     message = is_valid(message)
     if message == False:
         return 'Ваша ссылка некорректна'
-    elif len(message) >= 3:
+    elif message.index('.') >= 3:
         message = message[:3]
-    message = message + ''.join(random.sample(string.ascii_lowercase + string.digits, random.randint(1, 10)))
-    return 'tgshr.tk/' + message[:6]
+    else:
+        message = message[:message.index('.')]
+    message = message + ''.join(random.sample(string.ascii_lowercase + string.digits, random.randint(1, 2)))
+    return 'redurl.xyz/' + message
