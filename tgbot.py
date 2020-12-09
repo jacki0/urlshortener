e97155d70cd7b372ed47d76ddfd4862c4558fd04
jacki0 <jackio@tuta.io>
2020-12-09 20:47:39 +0300

    added a bot start message

--- tgbot.py
+++ tgbot.py
@@ -6,3 +6,13 @@ import json
 with open('config.json') as file:
     config = json.load(file)
     key = config['bot_key']
+
+bot = telebot.TeleBot(key)
+
+
+@bot.message_handler(commands = ['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "С помощью этого бота можно сократить вашу ссылку. \
+    \nОтправьте любую ссылку, в ответ на сообщение бот вернёт короткую ссылку с переадресацией на оригинальный адрес.")
+
+bot.polling()
