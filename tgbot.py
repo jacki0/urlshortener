80faad3e90125817cd1f23ab698cf4e975ae53f7
jacki0 <jackio@tuta.io>
2020-12-09 20:50:25 +0300

    added receipt and processing of the link and return of the shortened link

--- tgbot.py
+++ tgbot.py
@@ -15,4 +15,9 @@ def start_message(message):
     bot.send_message(message.chat.id, "С помощью этого бота можно сократить вашу ссылку. \
     \nОтправьте любую ссылку, в ответ на сообщение бот вернёт короткую ссылку с переадресацией на оригинальный адрес.")
 
+@bot.message_handler(content_types = ['text'])
+def askaction(message):
+    result = run(message)
+    bot.send_message(message.chat.id, result)
+
 bot.polling()
